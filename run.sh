3af95af247ca7dcea78be2fe1b47a4b1d29ad974
william <xiningbank@gmail.com>
2024-05-20 22:40:26 +0800

    add --no-tablespaces

--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@
 
 CURRENT_DATETIME=`date +"%Y-%m-%d-%H%M%S"`
 sudo mkdir -p $PATH_BACKUP
-docker exec $MYSQL_CONTAINER /usr/bin/mysqldump -u$USERNAME -p$PASSWORD $DATABASE | gzip > $PATH_BACKUP/${DATABASE}_${CURRENT_DATETIME}.sql.gz
+docker exec $MYSQL_CONTAINER /usr/bin/mysqldump --no-tablespaces -u$USERNAME -p$PASSWORD $DATABASE | gzip > $PATH_BACKUP/${DATABASE}_${CURRENT_DATETIME}.sql.gz
