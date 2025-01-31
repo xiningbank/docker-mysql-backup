6991e5c4abd967a581ea5d2262b238f43b0819fc
william_zhou <william_zhou@ehsy.com>
2025-01-31 20:13:58 +0800

    create directory by current date; change exported file name.

--- run.sh
+++ run.sh
@@ -2,6 +2,11 @@
 
 . .env
 
-CURRENT_DATETIME=`date +"%Y-%m-%d-%H%M%S"`
+CURRENT_DATE=`date +"%Y%m%d"`
+PATH_BACKUP="${PATH_BASE}/${CURRENT_DATE}"
 sudo mkdir -p $PATH_BACKUP
-docker exec $MYSQL_CONTAINER /usr/bin/mysqldump --no-tablespaces -u$USERNAME -p$PASSWORD $DATABASE | gzip > $PATH_BACKUP/${DATABASE}_${CURRENT_DATETIME}.sql.gz
+echo "will save to ${PATH_BACKUP}"
+
+CURRENT_DATETIME=`date +"%Y%m%d-%H%M%S"`
+docker exec $MYSQL_CONTAINER /usr/bin/mysqldump --no-tablespaces -u$USERNAME -p$PASSWORD $DATABASE   | gzip > $PATH_BACKUP/${CURRENT_DATETIME}_$DATABASE.sql.gz
+echo "backup database ${DATABASE} completed."
