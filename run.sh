93613ecaf06ae3a0e97377b8892c5e934b4c4f65
william_zhou <william_zhou@ehsy.com>
2025-01-31 22:28:13 +0800

    support export multiple databases; modify export file name; modify text.

--- run.sh
+++ run.sh
@@ -5,8 +5,8 @@
 CURRENT_DATE=`date +"%Y%m%d"`
 PATH_BACKUP="${PATH_BASE}/${CURRENT_DATE}"
 sudo mkdir -p $PATH_BACKUP
-echo "will save to ${PATH_BACKUP}"
+echo "will export to ${PATH_BACKUP}"
 
 CURRENT_DATETIME=`date +"%Y%m%d-%H%M%S"`
-docker exec $MYSQL_CONTAINER /usr/bin/mysqldump --no-tablespaces -u$USERNAME -p$PASSWORD $DATABASE   | gzip > $PATH_BACKUP/${CURRENT_DATETIME}_$DATABASE.sql.gz
-echo "backup database ${DATABASE} completed."
+docker exec $MYSQL_CONTAINER /usr/bin/mysqldump --no-tablespaces -u$USERNAME -p$PASSWORD --databases $DATABASE1 | gzip > $PATH_BACKUP/${CURRENT_DATETIME}_$FILENAME.sql.gz
+echo "backup database(s) completed."
